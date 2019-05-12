f20a203a9c1409e2a120d1d158c6b589fc3a1e7f
jen.yenpin <jen.yenpin@gmail.com>
2019-05-12 19:09:12 +0800

    ProductOrder

--- order_service/src/main/java/net/xdclass/order_service/controller/OrderController.java
+++ order_service/src/main/java/net/xdclass/order_service/controller/OrderController.java
@@ -1,6 +1,6 @@
 package net.xdclass.order_service.controller;
 
-import net.xdclass.order_service.domain.ProductOrder;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import net.xdclass.order_service.service.ProductOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/order")
@@ -15,14 +17,23 @@ public class OrderController {
     @Autowired
     private ProductOrderService productOrderService;
 
+    @HystrixCommand(fallbackMethod = "saveOrderFail")
     @RequestMapping("save")
-    public ProductOrder save(@RequestParam("user_id") int userId, @RequestParam("product_id") int productId) {
-        try {
-            return productOrderService.save(userId, productId);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
+    public Object save(@RequestParam("user_id") int userId, @RequestParam("product_id") int productId) throws IOException {
+        Map<Object, Object> data = new HashMap<>();
+        data.put("code", 0);
+        data.put("msg", productOrderService.save(userId, productId));
+        return data;
+    }
+
+
+    /* 注意，方法签名一定要要和api方法一致 */
+    public Map<String, Object> saveOrderFail(int useid, int productId) {
+        Map<String, Object> msg = new HashMap<>();
+        msg.put("code", -1);
+        msg.put("msg", "抢购人数太多，您被挤出来了，稍等重试");
+        return msg;
+
     }
 }
 
